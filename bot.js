83c74f32250ccdf99823d87b63c5f08453e93c61
uulltt <ajvark@gmail.com>
2018-10-13 22:52:46 -0400

    updating querybot

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const {
 } = require('pg');
 
 const herokupg = new Client({
-		connectionString: 'postgres://qyexdtumrzepev:34a0deb4187ea73ee9a6a6ee8e1b145c6140288c74432973049fa33d44907699@ec2-23-21-246-25.compute-1.amazonaws.com:5432/d3gtusutpn8b6n',
+		connectionString: 'postgres://txyddqffhajizp:99e64ff9f4120830980a80f77fda110906db27afa2b07c3832ab65e0851f7b08@ec2-23-23-80-20.compute-1.amazonaws.com:5432/d52kahtp6t26ms',
 		ssl: true,
 	});
 
